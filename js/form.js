af6c75f77b52a336b7347d39da33d30868e1391c
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-02-13 12:47:19 +0100

    moved popup css to its own file and made it responsive

--- js/form.js
+++ js/form.js
@@ -2,9 +2,13 @@ document.getElementById("name").addEventListener("input", function () {
   this.value = this.value.replace(/[^A-Za-zÀ-ÖØ-öø-ÿ\s]/g, "");
 });
 
-
-
 function redirectPopup() {
   console.log("Button clicked (inline script)");
   window.location.href = "http://127.0.0.1:5500/";
 }
+
+document.querySelector("#form").addEventmListener("submit", function (e) {
+  e.preventDefault();
+  console.log("Form submitted");
+  redirectPopup();
+});
--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,3 @@
 document.querySelector(".header_list").addEventListener("click", function () {
- document.querySelector(".checkbox").checked ^= 1;
-
-  console.log("Menu clicked");
+  document.querySelector(".checkbox").checked ^= 1;
 });
