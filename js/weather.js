049e4d0bd84270e1c85ae2cb5f49a6d20966f414
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-02-17 15:36:02 +0100

    finshed adding weather and made it responsive and fixed responsive issue with project cards

--- js/weather.js
+++ js/weather.js
@@ -1,4 +1,6 @@
 const weatherDiv = document.querySelector(".weather");
+const imgDiv = document.querySelector(".weather-img");
+const infoDiv = document.querySelector(".weather-info");
 
 async function getWeather() {
   const apiUrl =
@@ -7,19 +9,64 @@ async function getWeather() {
   try {
     const resp = await fetch(apiUrl);
     const data = await resp.json();
-
-    const currentTemperature = data.hourly.temperature_2m[0];
-    const currentCloudCover = data.hourly.cloudcover[0];
-    const rain = data.hourly.precipitation_probability[0];
-
-    
-    
-
-    console.log(`Current temperature in Härnösand: ${rain}°C`);
+    return data;
   } catch (error) {
     console.error("Error fetching weather for Härnösand:", error);
   }
 }
 
-getWeather();
-// cloudcover precipitation_probability time
+async function displayWeather() {
+  const data = await getWeather();
+
+  const currentTemperature = data.hourly.temperature_2m[0];
+  const currentCloudCover = data.hourly.cloudcover[0];
+  const rain = data.hourly.precipitation_probability[0];
+
+  const weatherConditions = [
+    { name: "Sunny", condition: currentCloudCover < 20 && rain < 10 },
+    {
+      name: "Cloudy",
+      condition: currentCloudCover >= 20 && currentCloudCover < 60 && rain < 10,
+    },
+    { name: "Cloud", condition: currentCloudCover >= 60 && rain < 10 },
+    { name: "Light Rain", condition: rain >= 10 && rain < 30 },
+    { name: "Rainy", condition: rain >= 30 && rain < 60 },
+    { name: "Heavy Rain", condition: rain >= 60 },
+  ];
+
+  const currentWeather = weatherConditions.find((weather) => weather.condition);
+
+  switch (currentWeather.name) {
+    case "Sunny":
+      imgDiv.innerHTML = '<img src="/images/weatherImages/sunny.png" alt="" />';
+      break;
+    case "Cloudy":
+      imgDiv.innerHTML =
+        '<img src="/images/weatherImages/cloudy.png" alt="" />';
+      break;
+    case "Cloud":
+      imgDiv.innerHTML = '<img src="/images/weatherImages/cloud.png" alt="" />';
+      break;
+    case "Light Rain":
+      imgDiv.innerHTML = '<img src="/images/weatherImages/rainy.png" alt="" />';
+      break;
+    case "Rainy":
+      imgDiv.innerHTML = '<img src="/images/weatherImages/rainy.png" alt="" />';
+      break;
+    case "Heavy Rain":
+      imgDiv.innerHTML = '<img src="/images/weatherImages/rain.png" alt="" />';
+      break;
+    default:
+      break;
+  }
+
+  infoDiv.innerHTML = `
+  Current temperature in Härnösand: ${currentTemperature}°C<br>
+  Cloud cover: ${currentCloudCover}% &nbsp;
+  Rain probability: ${rain}%
+`;
+
+  console.log(`Current temperature in Härnösand: °C`);
+}
+
+displayWeather();
