aa69b288b5cf863f55855e42fa9690c59c1cf9ea
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-02-17 12:08:06 +0100

    fixed the popup on the form

--- js/form.js
+++ js/form.js
@@ -1,14 +1,23 @@
-document.getElementById("name").addEventListener("input", function () {
+const form = document.querySelector("#form");
+const popupBtn = document.querySelector(".btn-popup");
+const popupDiv = document.querySelector(".popup");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const textAreaInput = document.getElementById("text-area");
+
+nameInput.addEventListener("input", function () {
   this.value = this.value.replace(/[^A-Za-zÀ-ÖØ-öø-ÿ\s]/g, "");
 });
 
-function redirectPopup() {
-  console.log("Button clicked (inline script)");
-  window.location.href = "http://127.0.0.1:5500/";
-}
-
-document.querySelector("#form").addEventmListener("submit", function (e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
+  nameInput.value = "";
+  emailInput.value = "";
+  textAreaInput.value = "";
+  popupDiv.classList.remove("hidden");
   console.log("Form submitted");
-  redirectPopup();
+});
+
+popupBtn.addEventListener("click", () => {
+  popupDiv.classList.add("hidden");
 });
